7569f74e505e18da33c2462193e7624562303d10
Shreyas Yadav <shreyasyadav11@gmial.com>
2023-03-24 11:03:46 +0530

    New

--- src/App.js
+++ src/App.js
@@ -1,46 +1,49 @@
-import './App.css';
-import Book from './users/Book';
-import './users/custom.css';
-import UserForm from './users/UserForm';
+import "./App.css";
+import Book from "./users/Book";
+import "./users/custom.css";
+import UserForm from "./users/UserForm";
 
 const objList = [
   {
-    name:"The Indian Metropolis",
-    auther: "Varun Gandhi",
-    addresslink:"https://images-eu.ssl-images-amazon.com/images/I/41cqBP3CKoL._AC_SX184_.jpg"
+    name: "The Indian Metropolis",
+    auther: "Varun Gandhi New",
+    addresslink:
+      "https://images-eu.ssl-images-amazon.com/images/I/41cqBP3CKoL._AC_SX184_.jpg",
   },
   {
-    name:"Never Never",
+    name: "Never Never",
     auther: "Colleen Hoover",
-    addresslink:"https://images-eu.ssl-images-amazon.com/images/I/51cUnd3JwoL._AC_SX184_.jpg"
+    addresslink:
+      "https://images-eu.ssl-images-amazon.com/images/I/51cUnd3JwoL._AC_SX184_.jpg",
   },
   {
-    name:"The Lord of the Rings",
+    name: "The Lord of the Rings",
     auther: "J. R. R. Tolkien",
-    addresslink:"https://images-eu.ssl-images-amazon.com/images/I/51myt0GXIUL._AC_SX184_.jpg"
+    addresslink:
+      "https://images-eu.ssl-images-amazon.com/images/I/51myt0GXIUL._AC_SX184_.jpg",
   },
   {
-    name:"Vahana",
+    name: "Vahana",
     auther: " Stuti Gupta",
-    addresslink:"https://images-eu.ssl-images-amazon.com/images/I/512URrvFNBL._AC_SX184_.jpg"
-  }
-]
+    addresslink:
+      "https://images-eu.ssl-images-amazon.com/images/I/512URrvFNBL._AC_SX184_.jpg",
+  },
+];
 
-
-const newArray = objList.map((element)=>{
+const newArray = objList.map((element) => {
   return (
     <>
-      <Book b1={element}/>
+      <Book b1={element} />
     </>
-  )
-})
+  );
+});
 
 function App() {
   return (
     <>
       <div className="App">
         {newArray}
-        <UserForm/>
+        <UserForm />
       </div>
     </>
   );
