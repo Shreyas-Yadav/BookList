e4b0e940f60a744b5bffe54a5a7ff89f3acb05ea
Shreyas Yadav <shreyasyadav11@gmail.com>
2023-03-24 10:52:18 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -5,32 +5,27 @@ import UserForm from './users/UserForm';
 
 const objList = [
   {
-    name:"Atomic Habits1",
-    auther: "shreyas1",
-    addresslink:"https://m.media-amazon.com/images/I/51YP7fM361S._SX460_BO1,204,203,200_.jpg"
+    name:"The Indian Metropolis",
+    auther: "Varun Gandhi",
+    addresslink:"https://images-eu.ssl-images-amazon.com/images/I/41cqBP3CKoL._AC_SX184_.jpg"
   },
   {
-    name:"Atomic Habits2",
-    auther: "shreyas2",
-    addresslink:"https://m.media-amazon.com/images/I/51YP7fM361S._SX460_BO1,204,203,200_.jpg"
+    name:"Never Never",
+    auther: "Colleen Hoover",
+    addresslink:"https://images-eu.ssl-images-amazon.com/images/I/51cUnd3JwoL._AC_SX184_.jpg"
   },
   {
-    name:"Atomic Habits3",
-    auther: "shreyas3",
-    addresslink:"https://m.media-amazon.com/images/I/51YP7fM361S._SX460_BO1,204,203,200_.jpg"
+    name:"The Lord of the Rings",
+    auther: "J. R. R. Tolkien",
+    addresslink:"https://images-eu.ssl-images-amazon.com/images/I/51myt0GXIUL._AC_SX184_.jpg"
   },
   {
-    name:"Atomic Habits4",
-    auther: "shreyas4",
-    addresslink:"https://m.media-amazon.com/images/I/51YP7fM361S._SX460_BO1,204,203,200_.jpg"
+    name:"Vahana",
+    auther: " Stuti Gupta",
+    addresslink:"https://images-eu.ssl-images-amazon.com/images/I/512URrvFNBL._AC_SX184_.jpg"
   }
 ]
 
-// const obj = {
-//   name:"Atomic Habits",
-//   auther: "shreyas",
-//   addresslink:"https://m.media-amazon.com/images/I/51YP7fM361S._SX460_BO1,204,203,200_.jpg"
-// }
 
 const newArray = objList.map((element)=>{
   return (
